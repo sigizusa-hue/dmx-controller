7378aa8d89bef7a086e63f94e2aa4bd9b3488f40
sigizusa-hue <sigizusa@gmail.com>
2026-06-21 00:26:39 +0000

    Add RTS test script

---
+++ test_rts.py
@@ -0,0 +1,25 @@
+from pyftdi.ftdi import Ftdi
+import time
+
+f = Ftdi()
+f.open_from_url('ftdi://ftdi:232r/1')
+f.reset()
+time.sleep(0.05)
+f.set_line_property(8, 2, 'N')
+f.set_flowctrl('')
+f.set_baudrate(250000)
+
+print('RTS=False, DTR=False - watch scope for 3 seconds...')
+f.set_rts(False)
+f.set_dtr(False)
+f.write_data(bytes([0x00] + [0xFF]*512))
+time.sleep(3)
+
+print('RTS=True, DTR=True - watch scope for 3 seconds...')
+f.set_rts(True)
+f.set_dtr(True)
+f.write_data(bytes([0x00] + [0xFF]*512))
+time.sleep(3)
+
+print('Done.')
+f.close()
